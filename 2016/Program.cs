81447ca75a954aba4d97a86ff96b1d2a34cdacbf
geldshot <geldshot@gmail.com>
2016-12-27 19:55:07 -0800

    Only commit

---
+++ 2016/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _2016
+{
+    /*
+    This year there were dragons.
+    Next year we'll be slaying.
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Fuck you 2016");
+        }
+    }
+}
